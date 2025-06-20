e61f994241b4e0b64d9e701e89b27b591ec39c47
porterbaker <jacksonpb@yahoo.com>
2025-06-20 13:00:18 -0500

    Initial project - 1.0

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,22 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title = 'Home')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -24,5 +24,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
